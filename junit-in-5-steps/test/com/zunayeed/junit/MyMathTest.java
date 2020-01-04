0552fa97323cabaa7a1482f31a0d75d7bdb0bd1a
Zunayeed Kamal <zunayeed.edu@gmail.com>
2020-01-03 20:18:51 -0500

    First Commit

---
+++ junit-in-5-steps/test/com/zunayeed/junit/MyMathTest.java
@@ -0,0 +1,14 @@
+package com.zunayeed.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyMathTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
